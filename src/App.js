43727b51335f43bb8a08927213f56fd2c01eccb2
Lena3891 <164846971+Lena3891@users.noreply.github.com>
2024-07-08 13:15:35 +0200

    Moved project from sandbox

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
